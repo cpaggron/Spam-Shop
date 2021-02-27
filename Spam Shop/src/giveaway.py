d12e4eb7e9e3be11833264e85aa79d87c73328c7
cpaggron <66983454+cpaggron@users.noreply.github.com>
2021-02-26 18:06:10 -0800

    Update giveaway.py

--- Spam Shop/src/giveaway.py
+++ Spam Shop/src/giveaway.py
@@ -29,20 +29,56 @@ if os.path.exists('bal.json'):
 else:
     bal = {}
 
+if os.path.exists('giveAmount.json'):
+    with open('giveAmount.json', 'r') as file:
+        giveAmount = json.load(file)
+else:
+    giveAmount = {}
+
+
+@client.command()
+async def add(ctx, amount: int):
+    if os.path.exists('bal.json'):
+        with open('bal.json', 'r') as file:
+            bal = json.load(file)
+    else:
+        bal = {}
+    user = str(ctx.author.id)
+    guild = str(ctx.guild.id)
+    if user in bal and bal[user] >= amount:
+        bal[user] -= amount
+        if guild in giveAmount:
+            giveAmount[guild] += amount
+        else:
+            giveAmount[guild] = amount
+        with open('bal.json', 'w+') as i:
+            json.dump(bal, i)
+        with open('giveAmount.json', 'w+') as a:
+            json.dump(giveAmount, a)
+    else:
+        await ctx.send('You can\'t afford that!')
+    
+
 @client.command()
 async def help(ctx):
-    embed = discord.Embed(title="Help", description="Here are the list of commands. Command prefix: `s!`", inline=False)
-    embed.add_field(name="Giveaway", value=)
+    embed = discord.Embed(title="Help", description="Here are the list of commands. Command prefix: `s!`", color=discord.Color.gold())
+    embed.add_field(name="Giveaway", value="To organize a giveaway", inline=False)
+    embed.add_field(name="Balance", value="To see your balance", inline=False)
+    embed.add_field(name="Gift [@user]", value="To gift a user Spambux", inline=False)
+    embed.add_field(name="Add [amount]", value="To add Spambux to your server's budget", inline=False)
+    embed.add_field(name="Budget", value="To view your server's budget. (If no one wins a giveaway the money won't be subtracted", inline=False)
+    embed.add_field(name="You can use the following links to spend your Spambux", value="[Spam Shop Server](https://discord.gg/AkYyFy5)", inline=False)
     await ctx.send(embed=embed)
 
 @client.command()
+@commands.has_role("Employee")
 async def give(ctx, amount: int, time: int):
     if os.path.exists('bal.json'):
         with open('bal.json', 'r') as file:
             bal = json.load(file)
     else:
         bal = {}
-    if ctx.author.id == 468476776104853505:
+    if ctx.guild.id == 733392111130640446:
         embed = discord.Embed(title="Spambux Giveaway", description=f"React to <:SpamShop:768583474206081054> for a chance to win {amount} <:Spambux:812017408260702248>!", color=discord.Color.blue())
         embed.add_field(name="Event ends in", value=f"{time} minutes", inline=False)
         embed.set_footer(text=f"Organized by {ctx.author.name}")
@@ -68,19 +104,21 @@ async def give(ctx, amount: int, time: int):
             win = winner.mention
             mark = '!'
             await ctx.send(f":tada: {win} has won the giveaway for {amount} <:Spambux:812017408260702248>! https://discord.com/channels/{ctx.guild.id}/{ctx.channel.id}/{msg.id}")
+            if str(winner.id) in bal:
+                bal[str(winner.id)] += amount
+            else:
+                bal[str(winner.id)] = amount
+            with open('bal.json', 'w+') as I:
+                json.dump(bal, I)
         else:
             win = "`No One`"
             mark = '.'
             await ctx.send(f":tada: {win} has won the giveaway for {amount} <:Spambux:812017408260702248>{mark} https://discord.com/channels/{ctx.guild.id}/{ctx.channel.id}/{msg.id}")
-        if str(winner.id) in bal:
-            bal[str(winner.id)] += amount
-        else:
-            bal[str(winner.id)] = amount
-        with open('bal.json', 'w+') as I:
-            json.dump(bal, I)
+    else:
+        pass
 
 @client.command(aliases=['g'])
-@commands.cooldown(1, 3600, commands.BucketType.user)
+@commands.has_permissions(administrator=True)
 async def giveaway(ctx, amount: int, time: int):
     if os.path.exists('badge.json'):
         with open('badge.json', 'r') as file:
@@ -108,6 +146,7 @@ async def giveaway(ctx, amount: int, time: int):
             await msg.edit(embed=embed1)
         gaw = await ctx.fetch_message(msg.id)
         users = await gaw.reactions[0].users().flatten()
+
         if len(users) > 3:
             N = 1
             while N == 1:
@@ -118,18 +157,19 @@ async def giveaway(ctx, amount: int, time: int):
                     break
             win = winner.mention
             mark = '!'
+            if str(winner.id) in bal:
+                bal[str(winner.id)] += amount
+            else:
+                bal[str(winner.id)] = amount
+            with open('bal.json', 'w+') as I:
+                json.dump(bal, I)
             await ctx.send(f":tada: {win} has won the giveaway for {amount} <:Spambux:812017408260702248>! https://discord.com/channels/{ctx.guild.id}/{ctx.channel.id}/{msg.id}")
         else:
             win = "`No One`"
             mark = '.'
             await ctx.send(f":tada: {win} has won the giveaway for {amount} <:Spambux:812017408260702248>! https://discord.com/channels/{ctx.guild.id}/{ctx.channel.id}/{msg.id}")
-        if str(winner.id) in bal:
-            bal[str(winner.id)] += amount
-        else:
-            bal[str(winner.id)] = amount
-        with open('bal.json', 'w+') as I:
-            json.dump(bal, I)
-    elif ctx.author.id == 468476776104853505:
+    
+    elif ctx.guild.id != 733392111130640446 and giveAmount[str(ctx.guild.id)] >= amount:
         embed = discord.Embed(title="Spambux Giveaway", description=f"React to <:SpamShop:768583474206081054> for a chance to win {amount} <:Spambux:812017408260702248>!", color=discord.Color.blue())
         embed.add_field(name="Event ends in", value=f"{time} minutes", inline=False)
         embed.set_footer(text=f"Organized by {ctx.author.name}")
@@ -154,17 +194,20 @@ async def giveaway(ctx, amount: int, time: int):
                     break
             win = winner.mention
             mark = '!'
+            giveAmount[str(ctx.guild.id)] -= amount
+            if str(winner.id) in bal:
+                bal[str(winner.id)] += amount
+            else:
+                bal[str(winner.id)] = amount
+            with open('bal.json', 'w+') as I:
+                json.dump(bal, I)
             await ctx.send(f":tada: {win} has won the giveaway for {amount} <:Spambux:812017408260702248>! https://discord.com/channels/{ctx.guild.id}/{ctx.channel.id}/{msg.id}")
         else:
             win = "`No One`"
             mark = '.'
             await ctx.send(f":tada: {win} has won the giveaway for {amount} <:Spambux:812017408260702248>{mark} https://discord.com/channels/{ctx.guild.id}/{ctx.channel.id}/{msg.id}")
-        if str(winner.id) in bal:
-            bal[str(winner.id)] += amount
-        else:
-            bal[str(winner.id)] = amount
-        with open('bal.json', 'w+') as I:
-            json.dump(bal, I)
+    elif giveAmount[str(ctx.guild.id)] < amount:
+        await ctx.send("You don't have enough Spambux in your budget! Use `s!buy [amount] to add to it!")
 
 @client.command(aliases=['bal'])
 async def balance(ctx, member : discord.Member=None):
@@ -187,6 +230,10 @@ async def balance(ctx, member : discord.Member=None):
             await ctx.send(f'{member.mention} has {bal[user]} <:Spambux:812017408260702248>!')
         else:
             await ctx.send(f'{member.mention} You have 0 <:Spambux:812017408260702248>.')
+
+@client.command()
+async def budget(ctx):
+    await ctx.send(f"{ctx.guild.name} has {giveAmount[str(ctx.guild.id)]} <:Spambux:812017408260702248> in its budget.")
 
 @client.command()
 async def gift(ctx, member : discord.Member, amount : int):
@@ -224,7 +271,7 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
         await ctx.send('Please put the required amount of arguments.')
     elif isinstance(error, commands.MissingPermissions):
-        await ctx.send(f'{ctx.author.mention} You are not allowed to used that!')
+        await ctx.send(f'{ctx.author.mention} You are not allowed to use that!')
     elif isinstance(error, commands.CommandOnCooldown):
         msg = "Try again in {:.2f} seconds. ".format(error.retry_after)
         embed = discord.Embed(title="You are on cooldown for this command!", description=msg, color=15158332)
@@ -235,5 +282,6 @@ async def on_command_error(ctx, error):
         pass
     else:
         raise error
+
 
 client.run("")
